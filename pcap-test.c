3bc2256fabbc9ef5a7283fc5f9b134f93ff44022
woodong-222 <ehddn2004@gmail.com>
2025-07-23 13:51:31 +0900

    Feat: packet capture

--- pcap-test.c
+++ pcap-test.c
@@ -1,6 +1,7 @@
 #include <pcap.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <netinet/in.h>
 #include "pcap-test.h"
 
 void usage() {
@@ -19,6 +20,65 @@ bool parse(Param* param, int argc, char* argv[]) {
 	}
 	param->dev_ = argv[1];
 	return true;
+}
+
+void capture_ethernet(const u_char* packet)
+{
+	EthernetHeader *ethernet = (EthernetHeader*)packet;
+
+	printf("src mac:\t");
+	for(uint8_t i = 0; i< ETHERNET_MAC_SIZE; i++)
+	{
+		printf("%02x", ethernet->source_mac_address[i]);
+		if(i < ETHERNET_MAC_SIZE - 1) printf(":");
+	}
+	printf("\n");
+
+
+	printf("dst mac:\t");
+	for(uint8_t i = 0; i< ETHERNET_MAC_SIZE; i++)
+	{
+		printf("%02x", ethernet->destination_mac_address[i]);
+		if(i < ETHERNET_MAC_SIZE - 1) printf(":");
+	}
+	printf("\n");
+}
+
+void capture_ipv4(const u_char* packet)
+{
+	Ipv4Header *ipv4 = (Ipv4Header*) packet;
+	uint32_t ip;
+	ip = ntohl(ipv4->source_address);
+	printf("src ip:\t\t%d.%d.%d.%d\n", (ip >> 24) & 0xFF, (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, ip & 0xFF);
+
+	ip = ntohl(ipv4->destination_address);
+	printf("dst ip:\t\t%d.%d.%d.%d\n", (ip >> 24) & 0xFF, (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, ip & 0xFF);
+}
+
+void capture_tcp(const u_char* packet)
+{
+	TcpHeader *tcp = (TcpHeader*) packet;
+
+	printf("src port:\t%d\n", ntohs(tcp->source_port));
+	printf("dst port:\t%d\n", ntohs(tcp->destination_port));
+}
+
+void capture_payload(const u_char* packet)
+{
+	printf("payload(data):\t");
+	for(int i = 0; i < 20; i++) {
+		printf("%02x ", packet[i]);
+	}
+	printf("\n");
+}
+
+void capture(const u_char* packet)
+{
+	capture_ethernet(packet);
+	capture_ipv4(packet + ETHERNET_SIZE);
+	capture_tcp(packet + ETHERNET_SIZE + IPV4_SIZE);
+	capture_payload(packet + ETHERNET_SIZE + IPV4_SIZE + TCP_SIZE);
+	printf("----------------------------------------\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -41,7 +101,7 @@ int main(int argc, char* argv[]) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
-		printf("%u bytes captured\n", header->caplen);
+		if ((*(uint16_t*)(packet + ETHERTYPE) == ntohs(IPV4)) && (*(uint8_t*)(packet+PROTOCOL) == TCP)) capture(packet);
 	}
 
 	pcap_close(pcap);
--- pcap-test.h
+++ pcap-test.h
@@ -1,98 +1,59 @@
+#pragma once
+
+#define ETHERNET_MAC_SIZE 6
+#define ETHERNET_SIZE 14
+#define IPV4_SIZE 20
+#define TCP_SIZE 20
+#define ETHERTYPE 12
+#define PROTOCOL 23
+#define IPV4 0x0800
+#define TCP 6
+
 typedef struct {
     char* dev_;
 } Param;
 
 typedef struct
 {
-    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
-    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
-    u_int16_t ether_type;                 /* protocol */
-} Ethernet_hdr;
+    u_int8_t    destination_mac_address[ETHERNET_MAC_SIZE];
+    u_int8_t    source_mac_address[ETHERNET_MAC_SIZE];
+    u_int16_t   ether_type;
+} EthernetHeader;
 
 typedef struct
 {
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t ip_hl:4,      /* header length */
-    ip_v:4;         /* version */
-#endif
-#if (LIBNET_BIG_ENDIAN)
-    u_int8_t ip_v:4,       /* version */
-    ip_hl:4;        /* header length */
-#endif
-    u_int8_t ip_tos;       /* type of service */
-#ifndef IPTOS_LOWDELAY
-#define IPTOS_LOWDELAY      0x10
-#endif
-#ifndef IPTOS_THROUGHPUT
-#define IPTOS_THROUGHPUT    0x08
-#endif
-#ifndef IPTOS_RELIABILITY
-#define IPTOS_RELIABILITY   0x04
-#endif
-#ifndef IPTOS_LOWCOST
-#define IPTOS_LOWCOST       0x02
-#endif
-    u_int16_t ip_len;         /* total length */
-    u_int16_t ip_id;          /* identification */
-    u_int16_t ip_off;
-#ifndef IP_RF
-#define IP_RF 0x8000        /* reserved fragment flag */
-#endif
-#ifndef IP_DF
-#define IP_DF 0x4000        /* dont fragment flag */
-#endif
-#ifndef IP_MF
-#define IP_MF 0x2000        /* more fragments flag */
-#endif
-#ifndef IP_OFFMASK
-#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
-#endif
-    u_int8_t ip_ttl;          /* time to live */
-    u_int8_t ip_p;            /* protocol */
-    u_int16_t ip_sum;         /* checksum */
-    struct in_addr ip_src, ip_dst; /* source and dest address */
-} Ipv4_hdr;
+    u_int8_t    version:4,
+                ihl:4;
+    u_int8_t    dscp:6,
+                ecn:2;
+    u_int16_t   total_length;
+    u_int16_t    identification;
+    u_int16_t   flags:3,
+                fragment_offset:13;
+    u_int8_t    time_to_live;
+    u_int8_t    protocol;
+    u_int16_t   header_checksum;
+    u_int32_t   source_address;
+    u_int32_t   destination_address;
+} Ipv4Header;
 
 typedef struct
 {
-    u_int16_t th_sport;       /* source port */
-    u_int16_t th_dport;       /* destination port */
-    u_int32_t th_seq;          /* sequence number */
-    u_int32_t th_ack;          /* acknowledgement number */
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t th_x2:4,         /* (unused) */
-    th_off:4;        /* data offset */
-#endif
-#if (LIBNET_BIG_ENDIAN)
-    u_int8_t th_off:4,        /* data offset */
-    th_x2:4;         /* (unused) */
-#endif
-    u_int8_t  th_flags;       /* control flags */
-#ifndef TH_FIN
-#define TH_FIN    0x01      /* finished send data */
-#endif
-#ifndef TH_SYN
-#define TH_SYN    0x02      /* synchronize sequence numbers */
-#endif
-#ifndef TH_RST
-#define TH_RST    0x04      /* reset the connection */
-#endif
-#ifndef TH_PUSH
-#define TH_PUSH   0x08      /* push data to the app layer */
-#endif
-#ifndef TH_ACK
-#define TH_ACK    0x10      /* acknowledge */
-#endif
-#ifndef TH_URG
-#define TH_URG    0x20      /* urgent! */
-#endif
-#ifndef TH_ECE
-#define TH_ECE    0x40
-#endif
-#ifndef TH_CWR
-#define TH_CWR    0x80
-#endif
-    u_int16_t th_win;         /* window */
-    u_int16_t th_sum;         /* checksum */
-    u_int16_t th_urp;         /* urgent pointer */
-} tcp_hdr;
+    u_int16_t source_port;
+    u_int16_t destination_port;
+    u_int32_t sequence_number;
+    u_int32_t acknowledgement_number;
+    u_int8_t    data_offset:4,
+                reserved:4;
+    u_int8_t    cwr:1,
+                ece:1,
+                urg:1,
+                ack:1,
+                psh:1,
+                rst:1,
+                syn:1,
+                fin:1;
+    u_int16_t window;
+    u_int16_t checksum;
+    u_int16_t urgent_pointer;
+} TcpHeader;
